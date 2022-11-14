82a24f796bf393a5db2d3b0c45ebbbd49ab0a3fc
Sno <gaby.durieux@gmail.com>
2022-11-15 00:46:19 +0100

    updated derivative.h Al'Hamdulillah

--- derivative.h
+++ derivative.h
@@ -6,9 +6,9 @@
 #define EXQUISITUS_C_ADAVERIS_DERIVATIVE_H
 
 typedef struct Derivative {
-    enum type { NOM, VER, ADJ, ADV, PRE, CON, PRO, DET, ABR, INT, ONO } type;
-    enum gender { MAS, FEM, INVGEN } gender;
-    enum tense { ENCULE } tense;
+    enum type { NOM, VER, ADJ, ADV, PREP, CONJ, PRON, DETR, ABRV, INTJ, ONOM } type;
+    enum gender { MAS, FEM, INVGEN, CARD } gender;
+    enum tense {IPRE, IPSIM, IIMP, PPRE, SIMP, PPAS, SPRE, IFUT, INF, CPRE, IMPRE, IMP}  tense;
     enum person { P1, P2, P3 } person;
     enum number { SG, PL, INVPL } number;
 } Derivative;
