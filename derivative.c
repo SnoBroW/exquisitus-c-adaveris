fdc7c5fbd94a4fb757f85a8f8859fe8af02c1ae3
Sno <gaby.durieux@gmail.com>
2022-11-16 22:15:56 +0100

    getGender, getNumber, getTense, getPerson from the form specifier

--- derivative.c
+++ derivative.c
@@ -35,7 +35,78 @@ enum type getType(char * form) {
         return ADV;
     } else if (strcmp(identifier, "Adj") == 0) {
         return ADJ;
+    } else {
+        return -1;
     }
-    // todo: the remaining ones
-    return -1;
+}
+
+
+enum gender getGender(char * gender) {
+    if (strcmp(gender, "Mas") == 0) {
+        return MAS;
+    } else if (strcmp(gender, "Fem") == 0) {
+        return FEM;
+    } else if (strcmp(gender, "InvGen") == 0) {
+        return INVGEN;
+    } else if (strcmp(gender, "Card") == 0) {
+        return CARD;
+    } else {
+        return -1;
+    }
+}
+
+
+enum number getNumber(char * number) {
+    if (strcmp(number, "SG") == 0) {
+        return SG;
+    } else if (strcmp(number, "PL") == 0) {
+        return PL;
+    } else if (strcmp(number, "InvPL") == 0) {
+        return INVPL;
+    } else {
+        return -1;
+    }
+}
+
+
+enum tense getTense(char * tense) {
+    if (strcmp(tense, "IPre") == 0) {
+        return IPRE;
+    } else if (strcmp(tense, "IPSim") == 0) {
+        return IPSIM;
+    } else if (strcmp(tense, "IImp") == 0) {
+        return IIMP;
+    } else if (strcmp(tense, "PPre") == 0) {
+        return PPRE;
+    } else if (strcmp(tense, "SImp") == 0) {
+        return SIMP;
+    } else if (strcmp(tense, "PPas") == 0) {
+        return PPAS;
+    } else if (strcmp(tense, "SPre") == 0) {
+        return SPRE;
+    } else if (strcmp(tense, "IFut") == 0) {
+        return IFUT;
+    } else if (strcmp(tense, "Inf") == 0) {
+        return INF;
+    } else if (strcmp(tense, "CPre") == 0) {
+        return CPRE;
+    } else if (strcmp(tense, "ImPre") == 0) { // impératifs des verbes LAMBDA, NULS
+        return IMPRE;
+    } else if (strcmp(tense, "Imp") == 0) { // imperatif des verbes pronominaux, les BG
+        return IMP;
+    } else {
+        return -1;
+    }
+}
+
+enum person getPerson(char * person) {
+    if (strcmp(person, "P1") == 0) {
+        return P1;
+    } else if (strcmp(person, "P2") == 0) {
+        return P2;
+    } else if (strcmp(person, "P3") == 0) {
+        return P3;
+    } else {
+        return -1;
+    }
 }
--- derivative.h
+++ derivative.h
@@ -4,7 +4,7 @@
 #include <string.h>
 
 typedef struct Derivative {
-    enum type { ADV, NOM, VER, ADJ, DETR, PRON, PREP, CONJ, ABRV, INTJ, ONOM } type;  // /!\ con =/= conj
+    enum type { ADV, NOM, VER, ADJ, DETR, PRON, PREP, CONJ, ABRV, INTJ, ONOM, QPRO } type;  // /!\ con =/= conj
     enum gender { MAS, FEM, INVGEN, CARD } gender;
     enum tense { IPRE, IPSIM, IIMP, PPRE, SIMP, PPAS, SPRE, IFUT, INF, CPRE, IMPRE, IMP }  tense;
     enum person { P1, P2, P3 } person;
@@ -14,6 +14,11 @@ typedef struct Derivative {
 
 Derivative * createDerivative();
 Derivative * processDerivative(char * form, char * word);
+
 enum type getType(char * form);
+enum gender getGender(char * gender);
+enum number getNumber(char * number);
+enum tense getTense(char * tense);
+enum person getPerson(char * person);
 
 #endif //EXQUISITUS_C_ADAVERIS_DERIVATIVE_H
