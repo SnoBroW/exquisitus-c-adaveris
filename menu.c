c95b98bed75616499dfee2136421183d979eead2
Sno <gaby.durieux@gmail.com>
2022-11-22 15:54:43 +0100

    là ça free tout hein faut faire un truc mieux sur les majuscules

--- menu.c
+++ menu.c
@@ -1,6 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include "menu.h"
 #include "derivative.h"
 #include "io.h"
@@ -36,7 +36,7 @@ void mainMenu() {
     int seed = initRand();
 
     // Dictionary * dict = initDict("dicts/dictionnaire_non_accentue.txt");
-    Dictionary * dict = initDict("dicts/dictionnaire.txt");
+    Dictionary * dict = initDict("../dicts/dictionnaire.txt");
 
     Node * node;
     DerivativeCell * derivativeCell;
@@ -201,22 +201,18 @@ bool isVowel(char c) {
 
 
 
-char * article(enum number number, enum gender gender, char firstLetter, bool capital) {
+char * article(enum number number, enum gender gender, char firstLetter) {
     int random = rand() % 2;
-    char * article;
+    char * article = NULL;
 
     if(number == PL) {
-        article = random ? "des" : "les";
+        article = (random ? "des" : "les");
     } else {
         if(gender == FEM) {
-            article = random ? "une" : isVowel(firstLetter) ? "l'" : "la";
+            article = (random ? "une" : isVowel(firstLetter) ? "l'" : "la");
         } else {
-            article = random ? "un" : isVowel(firstLetter) ? "l'" : "le";
+            article = (random ? "un" : isVowel(firstLetter) ? "l'" : "le");
         }
-    }
-
-    if(capital) {
-        article[0] = article[0] - 32;
     }
 
     return article;
@@ -235,7 +231,8 @@ void pattern1(Dictionary * dict, char * result) {
     char * ver = randomDerivation(dict, verd);
     char * nom2 = randomDerivation(dict, nom2d);
 
-    sprintf(result, "%s %s %s %s %s %s.", article(nom1d.number, nom1d.gender, nom1[0], true), nom1, adj, ver, article(nom2d.number, nom2d.gender, nom2[0], false), nom2);
+    sprintf(result, "%s %s %s %s %s %s.", article(nom1d.number, nom1d.gender, nom1[0]), nom1, adj, ver, article(nom2d.number, nom2d.gender, nom2[0]), nom2);
+    result[0] = result[0] - 32;
 }
 
 
@@ -252,7 +249,8 @@ void pattern2(Dictionary * dict, char * result) {
     char * nom2 = randomDerivation(dict, nom2d);
     char * adj = randomDerivation(dict, adjd);
 
-    sprintf(result, "%s %s qui %s %s %s %s %s.", article(nom1d.number, nom1d.gender, nom1[0], true), nom1, ver1, ver2, article(nom2d.number, nom2d.gender, nom2[0], false), nom2, adj);
+    sprintf(result, "%s %s qui %s %s %s %s %s.", article(nom1d.number, nom1d.gender, nom1[0]), nom1, ver1, ver2, article(nom2d.number, nom2d.gender, nom2[0]), nom2, adj);
+    result[0] = result[0] - 32;
 }
 
 
@@ -272,7 +270,8 @@ void pattern3(Dictionary * dict, char * result) {
     char * nom2 = randomDerivation(dict, nom2d);
     char * nom3 = randomDerivation(dict, nom3d);
 
-    sprintf(result, "Albane %s Gabriel pendant que %s %s %s %s %s %s sans %s.", ver1, article(nom1d.number, nom1d.gender, nom1[0], false), nom1, ver2, adv, article(nom2d.number, nom2d.gender, nom2[0], false), nom2, nom3);
+    sprintf(result, "Albane %s Gabriel pendant que %s %s %s %s %s %s sans %s.", ver1, article(nom1d.number, nom1d.gender, nom1[0]), nom1, ver2, adv, article(nom2d.number, nom2d.gender, nom2[0]), nom2, nom3);
+    result[0] = result[0] - 32;
 }
 
 void splashscreen(Dictionary * dict, char * result) {
@@ -285,5 +284,6 @@ void splashscreen(Dictionary * dict, char * result) {
 
     adv[0] = adv[0] - 32;
 
-    sprintf(result, "%s %s ? %s.", article(nomd.number, nomd.gender, nom[0], true), nom, adv);
+    sprintf(result, "%s %s ? %s.", article(nomd.number, nomd.gender, nom[0]), nom, adv);
+    result[0] = result[0] - 32;
 }
